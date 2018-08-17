4d9607721d5b8eb2fe8aae1d4c1aacfd6106895f
unknown <jailson.alves@quicard.com.br>
2018-08-17 18:35:30 -0300

    ajustando o nome do projeto

---
+++ SpringBootAngular/target/classes/sql/usuario.sql
@@ -0,0 +1,7 @@
+﻿create table usuario (idUsuario integer not null,
+		      email	character varying(60) not null,	
+		      nome	character varying(60),	
+		      ativo 	boolean,
+CONSTRAINT usuario_pkey PRIMARY KEY (idUsuario),
+CONSTRAINT usuario_uk unique (email)
+)
